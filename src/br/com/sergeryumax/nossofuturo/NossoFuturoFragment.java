dc49d61bc78856460419d5067e3684887db2f41b
Victor Ferme Gasparin <victor.f.gasparin@gmail.com>
2014-06-06 05:45:57 -0300

    Adicionando preview da camera

--- src/br/com/sergeryumax/nossofuturo/NossoFuturoFragment.java
+++ src/br/com/sergeryumax/nossofuturo/NossoFuturoFragment.java
@@ -3,10 +3,13 @@ package br.com.sergeryumax.nossofuturo;
 import android.app.Fragment;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.DisplayMetrics;
@@ -19,8 +22,10 @@ import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -36,14 +41,20 @@ public class NossoFuturoFragment extends Fragment implements OnClickListener, On
 	private Handler myHandler;
 	private View heartImg;
 	private View initialContent;
-	private View videoContent;
+	private RelativeLayout videoContent;
 	private VideoView videoPlayerComp;
 	private View video_content_mask;
+	private Camera mCamera;
+	private FrameLayout mPreviewLayer;
+	private FrameLayout mMicroPreviewLayer;
+	private View rootView;
+	private CameraPreview cameraPreview;
+	private Camera myCamera;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_nosso_futuro,
+		rootView = inflater.inflate(R.layout.fragment_nosso_futuro,
 				container, false);
 		
 		myHandler = new Handler();
@@ -70,7 +81,7 @@ public class NossoFuturoFragment extends Fragment implements OnClickListener, On
 		((ImageView)faceLeft).setImageBitmap(pesoaLeftRounded);
 		
 		Bitmap pessoaRight= BitmapFactory.decodeResource(getResources(), R.drawable.pessoa_left);
-		Bitmap pesoaRightRounded = ImageUtil.roundCornerImage(pessoaLeft, 360);
+		Bitmap pesoaRightRounded = ImageUtil.roundCornerImage(pessoaRight, 360);
 		
 		((ImageView)faceRight).setImageBitmap(pesoaRightRounded);
 		
@@ -79,10 +90,20 @@ public class NossoFuturoFragment extends Fragment implements OnClickListener, On
 		heightScreen = displaymetrics.heightPixels;
 		widthScreen = displaymetrics.widthPixels;
 		widthCenter = (widthScreen / 2) - 140;
+		
+		new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+		        mCamera = getCameraInstance();
+		        return null;
+			}
+		}.execute();
 	}
 
 	private void prepareVideoSection(View rootView) {
-		videoContent = rootView.findViewById(R.id.video_content);
+		mPreviewLayer = (FrameLayout) rootView.findViewById(R.id.camera_preview_layer);
+		mMicroPreviewLayer = (FrameLayout) rootView.findViewById(R.id.camera_preview_micro_layer);
+		videoContent = (RelativeLayout)rootView.findViewById(R.id.video_content);
 		video_content_mask = rootView.findViewById(R.id.video_content_mask);
 		
 		videoPlayerComp = (VideoView) rootView.findViewById(R.id.videoPlayerComponent);
@@ -125,16 +146,7 @@ public class NossoFuturoFragment extends Fragment implements OnClickListener, On
 		myHandler.postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				heartImg.animate().alpha(1.f).setDuration(500);
-//				heartImg.animate().rotationYBy(180).setDuration(1500).start();
-				
-//				ObjectAnimator.ofFloat(heartImg, "marginTop", (-1)*heightScreen/2).start();
-//				ObjectAnimator.ofFloat(heartImg, "marginBottom", (-1)*heightScreen/2).start();
-//				ObjectAnimator.ofFloat(heartImg, "marginLeft", (-1)*widthScreen/2).start();
-//				ObjectAnimator.ofFloat(heartImg, "marginRight", (-1)*widthScreen/2).start();
-				
-//				ObjectAnimator.ofInt(heartImg, "minimumHeight", heightScreen).start();
-//				ObjectAnimator.ofInt(heartImg, "minimumWidth", widthScreen).start();
+				heartImg.animate().alpha(1.f).setDuration(500).start();
 				
 				Animation resizeAnim = new ResizeAnimation(heartImg, (int)(heightScreen*0.8f), (int)(widthScreen*0.8f));
 				resizeAnim.setDuration(2000);
@@ -147,7 +159,7 @@ public class NossoFuturoFragment extends Fragment implements OnClickListener, On
 					@Override
 					public void onAnimationEnd(Animation animation) {
 						Animation resizeAnim = new ResizeAnimation(heartImg, 10, 10);
-						resizeAnim.setDuration(2000);
+						resizeAnim.setDuration(1300);
 						resizeAnim.setFillAfter(true);
 						resizeAnim.setInterpolator(new AccelerateDecelerateInterpolator());
 						resizeAnim.setAnimationListener(new AnimationListener() {
@@ -157,7 +169,8 @@ public class NossoFuturoFragment extends Fragment implements OnClickListener, On
 							public void onAnimationEnd(Animation animation) {
 								initialContent.setVisibility(View.GONE);
 								videoContent.setVisibility(View.VISIBLE);
-								startVideo();
+								video_content_mask.setVisibility(View.VISIBLE);
+								startVideoSection();
 							}
 						});
 						heartImg.startAnimation(resizeAnim);
@@ -169,10 +182,73 @@ public class NossoFuturoFragment extends Fragment implements OnClickListener, On
 		}, 1500);
 	}
 	
-	private void startVideo() {
+	private void startVideoSection() {
+        // Create our Preview view and set it as the content of our activity.
+        cameraPreview = new CameraPreview(getActivity(), mCamera);
+        mPreviewLayer.addView(cameraPreview);
+        
+		video_content_mask.animate().alpha(0f).setDuration(1000).withEndAction(new Runnable() {
+			@Override
+			public void run() {
+				myHandler.postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						video_content_mask.animate().alpha(1f).setDuration(300).withEndAction(new Runnable() {
+							@Override
+							public void run() {
+								mPreviewLayer.removeAllViews();
+								videoContent.removeView(mPreviewLayer);
+								mMicroPreviewLayer.addView(cameraPreview);
+								videoContent.requestLayout();
+								rootView.requestLayout();
+								video_content_mask.animate().alpha(0f).setDuration(500).withEndAction(new Runnable() {
+									@Override
+									public void run() {
+										startVideoPlayback();
+									}
+								}).start();
+							}
+						}).start();
+					}
+				}, 2500);
+			}
+		});
+	}
+	
+	@Override
+	public void onDestroyView() {
+		if (cameraPreview != null)
+			cameraPreview.stopPreviewAndFreeCamera();
+		super.onDestroyView();
+	}
+	private void startVideoPlayback() {
+		getActivity().findViewById(R.id.video_content_mask_in).setVisibility(View.VISIBLE);
+		videoPlayerComp.setVisibility(View.VISIBLE);
 		videoPlayerComp.requestFocus();
 		videoPlayerComp.start();
-		video_content_mask.animate().alpha(0f).setDuration(1000).start();
+		getActivity().findViewById(R.id.video_content_mask_in).animate().alpha(0f).setDuration(1000).start();
+	}
+	
+	/** A safe way to get an instance of the Camera object. */
+	public Camera getCameraInstance(){
+		int numberOfCameras = Camera.getNumberOfCameras();
+		for (int i = 0; i < numberOfCameras; i++) {
+			CameraInfo cameraInfo = new Camera.CameraInfo();
+			Camera.getCameraInfo(i, cameraInfo);
+			if (cameraInfo.facing == CameraInfo.CAMERA_FACING_FRONT){
+			    try {
+			        myCamera = Camera.open(i); // attempt to get a Camera instance
+			    }
+			    catch (Exception e){
+			        // Camera is not available (in use or does not exist)
+			    }
+			}
+		}
+		
+		if (myCamera == null){
+			myCamera = Camera.open();
+		}
+	    return myCamera; // returns null if camera is unavailable
 	}
 	
 	private void askQuestion() {
